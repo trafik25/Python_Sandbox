58660f3544cfd4f4840c7565e5e63e8c1564c692
Thomas Howard <thomashoward206@gmail.com>
2021-12-08 15:13:54 -0500

    Apostrophe

---
+++ apos.py
@@ -0,0 +1,10 @@
+
+
+
+# the book says single should create a syntax error but it doesnt. 
+message = 'This is a sentence written in Python for meoooo'
+print(message)
+
+
+message = "This is a sentence written in Python for meoooo"
+print(message)
