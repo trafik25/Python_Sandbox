ad125931bf2ab92598b609b251b105ea6290c8e7
Thomas Howard <thomashoward206@gmail.com>
2021-12-08 14:46:04 -0500

    Tab and Newline

---
+++ Tabbing_Newline.py
@@ -0,0 +1,11 @@
+# n is used for a newline to make lists
+var2 = 'Languages: \nPython\nJava\nC#Sharp'
+print(var2)
+
+
+var3 = 'Fruit: \ttomato\teggplant\tavacado'
+print(var3)
+
+#combining tab and newline 
+var3 = 'Fruit: \n\ttomato\n\teggplant\n\tavacado'
+print(var3)
--- Whitespace_Strings.py
+++ Whitespace_Strings.py
@@ -8,13 +8,4 @@ var = '\tpython'
 print(var)
 
 
-# n is used for a newline to make lists
-var2 = 'Languages: \nPython\nJava\nC#Sharp'
-print(var2)
 
-var3 = 'Fruit: \ntomato\neggplant\navacado'
-print(var3)
-
-#combining tab and newline 
-var3 = 'Fruit: \n\ttomato\n\teggplant\n\tavacado'
-print(var3)
