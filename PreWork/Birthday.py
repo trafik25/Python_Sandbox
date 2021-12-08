1742fcb11db914b2d504fce1bc93cdbcd624fa77
Thomas Howard <thomashoward206@gmail.com>
2021-12-08 15:42:23 -0500

    Last commit for today

--- PreWork/Birthday.py
+++ PreWork/Birthday.py
@@ -8,6 +8,8 @@ age = 23
 message = "Happy " + str(age) + "rd Birthday!"
 print(message)
 
+
+
 yards =  345
 message = "Tom Brady threw for " + str(yards) + " Yards on Monday Night for TB"
 print(message)
