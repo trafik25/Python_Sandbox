422aac49d89644c4c7febd70eabc07b5b9c4ea7b
Thomas Howard <thomashoward206@gmail.com>
2021-12-09 15:53:59 -0500

    End

--- PreWork/Chapter3/Deleting_listitems.py
+++ PreWork/Chapter3/Deleting_listitems.py
@@ -9,4 +9,13 @@ motorcycles = ['yamaha', 'ducati', 'harley', 'triumph ']
 del motorcycles[0]
 print(motorcycles)
 
+bdayList = ["Tom", "Bob" , "Todd" , "Mike"]
+print(bdayList)
+
+bdayList.append("Rob")
+print(bdayList)
+
+
+del bdayList[1]
+print(bdayList)
 
---
+++ PreWork/Chapter3/pop_method.py
@@ -0,0 +1,10 @@
+
+
+motorcycles = ['yamaha', 'ducati', 'harley', 'triumph ']
+print(motorcycles)
+
+
+#pop method removes the last item in a list
+popped_motorcycles = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycles)
