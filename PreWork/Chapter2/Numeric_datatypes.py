fb7e59eb0e3e0ba13d679c77f4c7f725484e2801
Thomas Howard <thomashoward206@gmail.com>
2021-12-09 11:42:21 -0500

    1

---
+++ PreWork/Chapter2/Numeric_datatypes.py
@@ -0,0 +1,15 @@
+
+
+
+# integers and floats 
+# squared ** (two stars)
+
+e = 5
+f =6
+y=7
+print(e)
+
+
+#medulo % (percent) operator
+# str is the string function assign a variable to a string
+# int is the integer function assign integer to a variable
