c056383d4c69bd5c88acf0905a5546cf6753487d
Thomas Howard <thomashoward206@gmail.com>
2021-12-09 16:01:03 -0500

    End

--- PreWork/Chapter3/pop_method.py
+++ PreWork/Chapter3/pop_method.py
@@ -5,6 +5,21 @@ print(motorcycles)
 
 
 #pop method removes the last item in a list
+#this comes in handy with chronological lists for items purchased
 popped_motorcycles = motorcycles.pop()
 print(motorcycles)
 print(popped_motorcycles)
+
+
+lastowned = motorcycles.pop()
+print("The Last motorcycle I owned was a " + lastowned.title() + '.')
+
+
+
+
+shopping = ['Nordstom', 'GAP', 'BR', 'Kmart']
+print(shopping)
+
+lastitem = shopping.pop()
+print("The last place I went shopping was " + lastitem.title() + '.')
+
