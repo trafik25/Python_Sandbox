497a15b7c6ab0ae436cbcb95cd637aa8c741157e
Thomas Howard <thomashoward206@gmail.com>
2021-12-09 15:46:09 -0500

    Insert

--- PreWork/Chapter3/Deleting_listitems.py
+++ PreWork/Chapter3/Deleting_listitems.py
@@ -1,3 +1,12 @@
 
 
-#deleting items using the 'del' function
+#deleting items using the 'del' statement
+
+#motorcycles = ['yamaha', 'ducati', 'harley', 'triumph ']
+#print(motorcycles)
+
+motorcycles = ['yamaha', 'ducati', 'harley', 'triumph ']
+del motorcycles[0]
+print(motorcycles)
+
+
