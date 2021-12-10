71470033a49c275ff03dcaf404203ad37f72349b
Thomas Howard <thomashoward206@gmail.com>
2021-12-10 01:44:47 -0500

    New

---
+++ PreWork/Chapter3/index_errors.py
@@ -0,0 +1,12 @@
+
+
+
+
+# will cause IndexError: list index out of range
+motorcycles = ['honda', 'yamaha', 'suzuki', ]
+print(motorcycles[0])
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles[-1])
+
