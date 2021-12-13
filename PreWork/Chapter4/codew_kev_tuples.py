7c75f6e76d5821da0a2ff490d63e49bbf9a5cafc
Thomas Howard <thomashoward206@gmail.com>
2021-12-13 15:37:21 -0500

    Slicing

---
+++ PreWork/Chapter4/codew_kev_tuples.py
@@ -0,0 +1,15 @@
+mytup = (1,2) 
+print(type(mytup))
+print(mytup)
+
+mytup[1]
+print(mytup[1])
+
+mytup = mytup + (3,4)
+print(mytup)
+
+#slicing
+mystring = "hey guys lets learn python"
+mylist = ['pizza', 'tacos', 'dimsum' , 'sushi']
+
+print(mystring[4:7])
