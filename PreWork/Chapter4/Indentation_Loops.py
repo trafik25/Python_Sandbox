1bca5e9edc4ee0c0dc93885fafd08fb9cf9e1151
Thomas Howard <thomashoward206@gmail.com>
2021-12-13 10:29:13 -0500

    Loops

--- PreWork/Chapter4/Indentation_Loops.py
+++ PreWork/Chapter4/Indentation_Loops.py
@@ -11,3 +11,22 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician.title() + ", that was a great trick!")
 print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+#this will cause an error because the print function doesnt belong to the line above it. 
+#message = "Hello Python world!" 
+#   print(message)
+
+
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+       print(magician.title() + ", that was a great trick!")
+       print("I can't wait to see your next trick, " + magician.title() + ".\n")
+       print("Thank you everyone, that was a great magic show!")
+
+
+
+# without the colon, the interpreter prints an error that the colon is expected
+magicians = ['alice', 'david', 'carolina'] 
+for magician in magicians
+print(magician)
