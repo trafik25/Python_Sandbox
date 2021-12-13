b5dd8ea59b95beb66ae2992a8492a818916500eb
Thomas Howard <thomashoward206@gmail.com>
2021-12-13 14:34:44 -0500

    Loops

---
+++ PreWork/Chapter4/codew_kev_loops.py
@@ -0,0 +1,18 @@
+
+#training with Kevin
+foods = ['tacos', 'pizza', 'sushi', 'mcds']
+print(foods[0])
+
+#for PLACEHOLDER in THELISTWEWANT: 
+    #code block goes here
+    #COLONs specify a code block
+
+for food in foods:
+    
+    if food == "pizza": 
+        continue   
+    print(f"I like {food} because they are tasty!")
+
+
+
+
