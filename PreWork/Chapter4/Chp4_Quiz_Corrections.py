6b4a29fe3b580c81bd3a4313f294a541a59a45e3
Thomas Howard <thomashoward206@gmail.com>
2021-12-14 17:35:18 -0500

    Quiz

---
+++ PreWork/Chapter4/Chp4_Quiz_Corrections.py
@@ -0,0 +1,20 @@
+#Questions I got wrong reviewed
+#list.reverse
+#reverse sorts a list in reverse alphabetical order
+list = ['Tom', 'Wayne', 'Bob', 'Charles']
+list.sort(reverse=True)
+print(list)
+
+
+#Sorted
+#sorted sorts by order of the list 
+names = ['Tom', 'Wayne', 'Bob', 'Charles']
+sorted(names)
+print(names)
+
+
+#append----Append adds whatever is in the parens no matter what is there, quotations or not, string or integer
+names = ['Tom', 'Wayne', 'Bob', 'Charles']
+names.append(4)
+print(names)
+
