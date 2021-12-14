c2bd0f59961defd5a0e9b4ef6c5a42a7d8c6e709
Thomas Howard <thomashoward206@gmail.com>
2021-12-14 15:12:28 -0500

    Review

--- PreWork/Chapter4/Chp4_VidReview.py
+++ PreWork/Chapter4/Chp4_VidReview.py
@@ -1,3 +1,26 @@
+#Index      0        1          2        3
+foods = ['pizza', 'tacos', 'dim sum', 'sushi']  #foods are objects in my list
+
+#listname [index]
+for food in foods: 
+    if food == 'dim sum': 
+       continue   
+    print(f"I like {food} because they are yummy")
+
+#List the foods by my fav in order from fav to least fav
+for food in foods: 
+    if food == 'dim sum': 
+       continue   
+    print(f"I like {food} because they are yummy")
 
 
-#Review of Video 
+for index in range(len(foods)):
+    print(index)
+    print(foods[index])
+
+#for index, food in enumerate(foods):
+
+print(list(enumerate(foods)))
+for index, food in enumerate(foods):
+    print(f"My No {index + 1} food is {food.title()}")
+    
