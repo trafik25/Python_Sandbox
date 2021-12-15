fc51c2e33f2ee50be00473602321995425cec2e9
Thomas Howard <thomashoward206@gmail.com>
2021-12-15 15:39:02 -0500

    If

--- PreWork/Chapter5/Boolean_expressions.py
+++ PreWork/Chapter5/Boolean_expressions.py
@@ -4,6 +4,17 @@
 #Boolean values are often used to keep track of certain conditions, such as 
 # whether a game is running or whether a user can edit certain content on a website:
 
+
+
+##BOOLEAN EXPRESSION
+a = 200
+b = 337
+
+if b != a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a") 
+
 car = 'subaru'
 print("Is car == 'subaru'? I predict True.")
 print(car == 'subaru')
@@ -11,3 +22,9 @@ print(car == 'subaru')
 print("\nIs car == 'audi'? I predict False.")
 print(car == 'bmw')
 
+employees = ['Susan', 'Tolly', 'Thomas', 'Frank', 'Jared', 'Tony']
+for people in employees:
+    if people in employees == 'Susan':
+        print("Susan is our best employee!")
+    else:
+        print("No information found")
--- PreWork/Chapter5/if_statements.py
+++ PreWork/Chapter5/if_statements.py
@@ -10,5 +10,26 @@ for car in cars:
     else:
         print(car.title())
 
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+for topping in requested_toppings:
+    if topping == 'tuna':
+        print(topping.title())
+    else:
+        print(topping.upper())
+
+my_cars = ['bmw', 'toyota', 'ferrari', 'pugeot', 'nissan']
+for car in cars:
+    if car == 'ferrari':
+        print("That's a fancy car!")
+    else:
+        print("The car was too expensive")
 
 
+banned_users = ['Tori', 'Larry', 'Juan', 'Marco']
+for user in banned_users:
+    if user == 'Larry':
+        print("You are banned")
+    else:
+        print("You may continue playing!")
+
+        
