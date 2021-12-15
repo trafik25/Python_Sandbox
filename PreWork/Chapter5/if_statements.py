617ddb003b852dffc9317d73b2efb7e537a91c75
Thomas Howard <thomashoward206@gmail.com>
2021-12-15 13:28:49 -0500

    Chp 5

---
+++ PreWork/Chapter5/if_statements.py
@@ -0,0 +1,13 @@
+
+##IF STATEMENTS
+#Imagine you have a list of cars and you want to print out the name of each car. 
+
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+#####Conditional tests
