e179f6d933ae8a95bd17c464f9ec13f275d47eaf
Thomas Howard <thomashoward206@gmail.com>
2021-12-15 15:44:39 -0500

    Else if

--- PreWork/Chapter5/if_statements2.py
+++ PreWork/Chapter5/if_statements2.py
@@ -1,3 +1,13 @@
 
 
 #IF STATEMENTS
+#IF else statements comparing a value and printing outcome based on answer
+
+age = 16
+if age >= 18:
+    print("You are old enough to vote")
+    print("Have you registered for the coming election?")
+else:
+    print("You are not elgible to vote yet")
+    print("Remember to register to vote when you turn 18 years of age")
+
