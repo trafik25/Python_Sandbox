5707dfa864dcae4f8399270914c82182068c3c90
Thomas Howard <thomashoward206@gmail.com>
2021-12-15 14:12:43 -0500

    5

--- PreWork/Chapter5/Boolean_expressions.py
+++ PreWork/Chapter5/Boolean_expressions.py
@@ -4,3 +4,10 @@
 #Boolean values are often used to keep track of certain conditions, such as 
 # whether a game is running or whether a user can edit certain content on a website:
 
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'bmw')
+
