46434558ad3dede50804234af42f8cde79932934
Thomas Howard <thomashoward206@gmail.com>
2021-12-16 14:29:24 -0500

    test

--- PreWork/Chapter5/testing_Ifstatement.py
+++ PreWork/Chapter5/testing_Ifstatement.py
@@ -24,9 +24,58 @@ if  alien_color == 'green':
 elif alien_color == 'purple':
     print("You earned 25 pts")
 
+#green alien
+alien_color = 'green'
+
+if  alien_color == 'green':
+    print("You just earned 5 points")
+elif alien_color == 'yellow':
+    print("You just earned 10 pts")
+elif alien_color == 'red':
+    print("You just earned 15 pts")
+else:
+    alien_color = 'orange'
+    print("You just earned 50 pts")
 
 
+#yellow alien
+alien_color = 'yellow'
+
+if  alien_color == 'green':
+    print("You just earned 5 points")
+elif alien_color == 'yellow':
+    print("You just earned 10 pts")
+elif alien_color == 'red':
+    print("You just earned 15 pts")
+else:
+    alien_color = 'orange'
+    print("You just earned 50 pts")
 
 
+#red alien
+alien_color = 'red'
+
+if  alien_color == 'green':
+    print("You just earned 5 points")
+elif alien_color == 'yellow':
+    print("You just earned 10 pts")
+elif alien_color == 'red':
+    print("You just earned 15 pts")
+else:
+    alien_color = 'orange'
+    print("You just earned 50 pts")
+
+#missing color alien
+alien_color = 'tan'
+
+if  alien_color == 'green':
+    print("You just earned 5 points")
+elif alien_color == 'yellow':
+    print("You just earned 10 pts")
+elif alien_color == 'red':
+    print("You just earned 15 pts")
+else:
+    alien_color = 'blue'
+    print("You just earned 50 pts")
 
 
---
+++ test.py
@@ -0,0 +1,20 @@
+
+
+#lifespans
+#< less than like an L, > greater than 
+age = 14
+
+if   age < 2:
+    print("This person is a baby")
+elif age < 4:
+    print("This person is a toddler")
+elif age < 13:
+    print("this person is a child")
+elif age < 19:
+    print("This person is a teenager")
+elif age < 64:
+    print("This person is an adult")
+else:
+    age < 65
+    print("This person is a senior citizen.")
+
