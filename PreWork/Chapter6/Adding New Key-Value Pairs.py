df2b3488f8e11e76baacd8f52e01b454afbc8633
Thomas Howard <thomashoward206@gmail.com>
2021-12-18 15:07:27 -0500

    Test

--- PreWork/Chapter6/Adding New Key-Value Pairs.py
+++ PreWork/Chapter6/Adding New Key-Value Pairs.py
@@ -1,2 +1,9 @@
 
 
+car_0 = {'color': 'grey', 'trim_package': 'SL', 'engine': '5.5' }
+print(car_0)
+
+#coordinates of the car on the screen 
+car_0['x_position'] = 0
+car_0['y_position'] = 25
+print(car_0)
---
+++ PreWork/Chapter6/Starting with an Empty Dictionary.py
@@ -0,0 +1,16 @@
+
+#This is an empty dictionary
+#WHY USE THIS?? 
+
+
+#Typically, you’ll use empty dictionaries when storing user-supplied data in a dictionary or 
+# when you write code that generates a large number of key-value pairs automatically.
+car_0 = { }
+
+car_0['color'] = 'grey'
+car_0['engine'] = '5.5L'
+
+print(car_0)
+
+
+
