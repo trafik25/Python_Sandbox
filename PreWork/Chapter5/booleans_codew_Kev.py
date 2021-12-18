e9c6cd90077560b791826ffaf9d9f18fe6e12218
Thomas Howard <thomashoward206@gmail.com>
2021-12-18 10:38:09 -0500

    Bool

---
+++ PreWork/Chapter5/booleans_codew_Kev.py
@@ -0,0 +1,29 @@
+
+
+#Boolean can be two things (binary operator) it can be on/off, yes or no, true or false
+
+#0 is always false, everything else is true
+print(bool(0))
+print(bool(""))  # empty string always false
+print(bool(" trs"))  #occupied string is always true
+print(bool([1,2]))  # occupied list is always true
+print(bool([ ]))  # empty list is always false
+
+#boolean expressions have operators
+# > greater than <less than 
+# >= 
+# ==  equal to
+# !# not equal to
+
+print("dog" == "cat")
+print("dog" == "dog")
+print(4 > 8)
+
+x = 3
+print(5 < x < 20)  # 10 is betwen 5 and 20, 3 is not
+
+letter = "c"
+print(ord("b"))
+print(letter > "a")
+
+
--- PreWork/Chapter5/codew_Kev.py
+++
@@ -1,6 +0,0 @@
-
-
-#Boolean can be two things (binary operator) it can be on/off, yes or no, true or false
-
-
-print(bool(0))
