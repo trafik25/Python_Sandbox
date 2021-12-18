cd10794e1483cc263c911a0d4bf2d2d2bc4f938d
Thomas Howard <thomashoward206@gmail.com>
2021-12-18 12:52:07 -0500

    Dct

---
+++ PreWork/Chapter6/simpledictionary.py
@@ -0,0 +1,11 @@
+
+
+#Dictonaries store info on people or things
+#A dictionary in Python is a collection of key-value pairs. Each key is connected to a value
+# dictionaries are wrapped in braces { }
+# Every key is connected to its value by a colon,
+
+car_0 = {'color': 'grey', 'trim_package': 'SL', 'engine': '5.5' }
+print(car_0['color'])
+print(car_0['trim_package'])
+print(car_0['engine'])
