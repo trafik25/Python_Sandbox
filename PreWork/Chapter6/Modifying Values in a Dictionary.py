f86c39d786ea07e5326d8280f3093e54d8ba9fd7
Thomas Howard <thomashoward206@gmail.com>
2021-12-18 15:19:12 -0500

    Chp 6

--- PreWork/Chapter6/Modifying Values in a Dictionary.py
+++ PreWork/Chapter6/Modifying Values in a Dictionary.py
@@ -1,13 +1,13 @@
 
 
-car_0 = {'color': 'grey', }
-print("The car color is " + car_0['color']+ ".")
- #Change the color of the car
-car_0 = {'color': 'canary yellow'}
-print("The car has been repainted " + car_0['color'] + '.')
+
 
 car_0 = {'color': 'grey', 'trim_package': 'SL', 'engine': '5.5L' }
+print("The car is " + car_0['color'])
+
 car_0 = {'engine': '6.5L'}
-car_0 = {'trim_package': 'SLC'}
-print("The car's engine has been upgraded to " + car_0['engine'])
-print("The cars new interior package is " + car_0['trim_package'] + '.')
+print("The car engine has been upgraded to  " + car_0['engine'] + '.')
+
+car_0 = {'trim_package': 'SLTx'}
+print("The car trim has been upgraded to  " + car_0['trim_package'] + '.')
+
