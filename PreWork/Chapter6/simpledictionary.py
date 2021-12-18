2c063234e686aac082e1b0f80eea8ddc06e569e2
Thomas Howard <thomashoward206@gmail.com>
2021-12-18 13:55:48 -0500

    Chp 6

--- PreWork/Chapter6/simpledictionary.py
+++ PreWork/Chapter6/simpledictionary.py
@@ -16,3 +16,14 @@ car_0 = {'color': 'grey', 'trim_package': 'SL', 'engine': '5.5' }
 print(car_0['color'])
 
 #You can have an unlimited number of key-value pairs in a dictionary.
+
+# FORMAT
+# Var = {'KEY', 'VALUE': 'KEY', 'VALUE': 'KEY', 'VALUE':}
+# print(key['value'])
+
+car_0 = {'color': 'grey', 'trim_package': 'SL', 'engine': '5.5' }
+
+new_car = car_0['engine']
+print("This car has a " + str(new_car) + " liter engine in it.")
+new_car_color = car_0['color']
+print("This car comes with a " + str(new_car_color) + " color package.")
