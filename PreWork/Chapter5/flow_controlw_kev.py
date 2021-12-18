a6c5bdb6ac7cf0c101994ae3c8335459ee79e852
Thomas Howard <thomashoward206@gmail.com>
2021-12-18 11:05:20 -0500

    test

--- PreWork/Chapter5/flow_controlw_kev.py
+++ PreWork/Chapter5/flow_controlw_kev.py
@@ -1,4 +1,20 @@
 
 
 #flow control 
+#if BOOLEAN OR BOOLEAN EXPRESSION
+    #CODE BLOCK
+    # FOR IF TRUE
+#else:
+    #for the if false code block
+
+
+height =  77
+if height < 60:
+    print("You are too short")
+elif height > 72:
+    print("You are too tall")
+else:
+    print("You are tall enough")
+print("Thanks for visiting")
+
 
