29c15ae9e0943aba617242aa0e7ca33e62599dfb
Thomas Howard <thomashoward206@gmail.com>
2021-12-19 17:26:48 -0500

    6

---
+++ PreWork/Chapter6/looping_through_ALLVALS.py
@@ -0,0 +1,26 @@
+
+
+fav_lang = { 
+        'Steph': 'C#',
+        'Rob': 'python',
+        'Jeff': 'HTML',
+        'Sara': 'CSS',
+}
+#Sorting by alphabetical order
+for name in sorted(fav_lang.keys()):
+       print(name + ", thank you for taking the poll.")
+
+person = { 
+        'firstname': 'Ron',
+        'lastname': 'Howard',
+        'phone_number': 2068527023 ,
+        'location': 'Whidbey Island' , 
+        'height' : 75,
+        'weight': 200,
+        'eyes' : 'blue',
+        'occupation': 'loan officer',
+}
+
+for info in sorted(person.keys()):
+    print(info)
+
