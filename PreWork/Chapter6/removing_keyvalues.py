70076170dbdd6f7add0217517582aee8dffe7fe7
Thomas Howard <thomashoward206@gmail.com>
2021-12-19 13:10:44 -0500

    6

---
+++ PreWork/Chapter6/removing_keyvalues.py
@@ -0,0 +1,11 @@
+
+
+alien_0 = {'color': 'red', 'shape' : 'square' ,'points': 15}
+
+#deleting it from the dictionary
+del alien_0['color']
+print(alien_0)
+
+#adding it back to the dictionary
+alien_0['color']= 'red'
+print(alien_0)
