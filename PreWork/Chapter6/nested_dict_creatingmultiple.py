f3691e9935af76f63aeabb6e96847a4558dca67a
Thomas Howard <thomashoward206@gmail.com>
2021-12-20 12:22:30 -0500

    6

--- PreWork/Chapter6/nested_dict_creatingmultiple.py
+++ PreWork/Chapter6/nested_dict_creatingmultiple.py
@@ -6,17 +6,22 @@ aliens = []
 
 # Make 15 green aliens using range().
 for alien_number in range(15):
-    new_alien = {'color': 'red', 'points': 15, 'speed': 'slow'}
+    new_alien = {'color': 'green', 'points': 15, 'speed': 'slow'}
     aliens.append(new_alien)
 
-for alien in aliens[0:3]:
-    if alien['color'] == 'red':
+for alien in aliens[0:4]:
+    if alien['color'] == 'green':
         alien['color'] = 'yellow'
         alien['speed'] = 'medium'
         alien['points'] = 20
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 35
+    
 
 # Show the first 5 aliens using a slice[:5]
-for alien in aliens[:7]:
+for alien in aliens[:8]:
        print(alien)
 print("...")
 
